c54f5c5cf3d84624d9c7f0b943d37362266e994b
Staggolee <davitiani.dato@gmail.com>
2018-06-27 21:41:11 +0400

    arrays

---
+++ 27.06/index.js
@@ -0,0 +1,89 @@
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+// arr.forEach((element, index, arr) => {  //3 argumentiania
+//console.log(index)
+//console.log(`value -${element},index - ${index}, array -${arr}`);
+// })
+
+// let newArr = arr.map((element, index, arr) => {
+//     return `<div><b>${element*2}</b></div>`;
+// });
+
+//console.log(arr);
+//document.body.innerHTML = newArr.toString().split(',').join('');
+//console.log(newArr.toString().split(','));
+
+// let filtered = arr.filter((element)=>{
+//     return element & 1;
+// });
+// console.log(filtered);
+// for(let entries of arr.entries()){
+//     console.log(entries);
+// }
+
+// console.log(arr.entries());
+// console.log(arr.values());
+// console.log(arr.keys());
+
+// Object.keys()
+// object.entries()
+
+
+// let myArray = ["Jazz", "Blues"]
+// console.log(myArray);
+
+// myArray.push("Rock-n-Roll")
+// console.log(myArray);
+
+// myArray[Math.floor(myArray.length/2)] = "classic"
+// console.log(myArray);
+// myArray.shift();
+
+// console.log(myArray);
+
+// myArray.unshift("Rap", "Reggae")
+
+// console.log(myArray);
+
+
+// let arr = [-2,-3,4,-1,-2,1,5,-3];
+// // console.log(arr.length);  8 
+
+// function allSum(x,y, ...numbers) {
+//     // let xy= x+y;                 //
+//     // for(let item of arguments){   //...numbers-tan es nawili ar mchirdeba
+//     //     xy += item;             // 
+//     // }                            //
+//     console.log(numbers);
+//     // console.log(...arguments)
+
+// }
+// // allSum(1,2,3,4,5,6);
+
+// console.log(Math.max(...[1,2,3,4,5,6]));
+
+
+// let merge = [100,113,...[1,2,3,4,5,6], ...arr,65];
+// console.log(merge)
+// let str = "georgia";
+// console.log([...str]);
+
+// let elementNode = document.createElement('strong');
+// let elementText = document.createTextNode('georgia');
+
+// let containerDiv = document.querySelector('#container');
+// let p = containerDiv.querySelector('#para');
+
+// p.appendChild(elementNode);
+// elementNode.appendChild(elementText);
+
+// console.log(document.body.innerHTML);
+
+//insert before (elementNode, element)
+
+// generateHTML(count, tagName, Selector)
+//generateHTML(1, 'ul', '#container', 3, 'li');
+
+
+
+

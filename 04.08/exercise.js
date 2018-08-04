7af4467e731a024cf68404fb7176a1a523ef1f83
Staggolee <davitiani.dato@gmail.com>
2018-08-04 16:21:07 +0400

    შაბათის ჯოჯოხეთი

---
+++ 04.08/exercise.js
@@ -0,0 +1,43 @@
+// let myInfo = (function (){
+//     let saveName = 'your name';
+//     let saveSurname = 'your surname';
+//     let saveAge = 'your age';
+
+//     let $element = $('info');
+//     let $input = $element.find('input');
+//     let $button = $element.find('save');
+//     let $buttonS = $element.find('show');
+//     let $show = $element.find('saveInfo')
+    
+//     $button.on('click', addInfo);
+//     $show.delegate('.showInfo', 'click', showInfo);
+
+
+//     _createStorage();
+//     _getStorage();
+//     _updateDom();
+
+//     function addName(newN){
+//         let name = (typeof newVal === 'string') ? newVal : $input.val();
+//         people.push(name);
+//         _updateStorage();
+//         _updateDOM();
+//         $input.val('');
+
+//     }
+
+//     return {
+//         addName
+//     }
+
+
+// })
+
+let numbers = (function() {
+    let firstS = 'first'
+    let spans = $("span").append("span");
+    
+    
+    
+})
+console.log(numbers)
